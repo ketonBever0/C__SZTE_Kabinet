daf9f6948669535dd5994db3999129dfd1efdf1f
Kurucz Laci <lacikurucz09@gmail.com>
2023-12-09 15:21:04 +0100

    hazi10 próba folyt

--- biro2/hazi10/feladat.c
+++ biro2/hazi10/feladat.c
@@ -12,15 +12,52 @@ int main()
         return -1;
     }
 
-    char **sor = malloc(sizeof(char *));
+    int kezdoSor, kezdoOszlop;
+    if (fscanf(fBe, "%d,%d", &kezdoSor, &kezdoOszlop) != 2)
+    {
+        printf("Nem olvashatoak a kezdo koordinatak!");
+        return -2;
+    }
+
+    char *tomb = malloc(sizeof(int));
 
     /* char *sor = malloc(sizeof(char)); */
 
-    for (int i = 0;; i++)
+    int vege = 0;
+    int arrLen = 0;
+    for (int i = 0; vege == 0; i++)
     {
-        sor[i] = malloc(sizeof(char));
-        fscanf(fBe, "%c", sor[i]);
+        char *sor = malloc(sizeof(char));
+        fscanf(fBe, "%s", sor);
+        for (int j = 0; sor[j] != '\0'; j++)
+        {
+            char fscan;
+            fscanf(fBe, "%c", &fscan);
+
+            if (fscan == ',')
+            {
+                continue;
+            }
+            else if (fscan == '-')
+            {
+                vege = 1;
+                break;
+            }
+            else
+            {
+                tomb[i] = (int)fscan;
+                arrLen++;
+            }
+        }
+        free(sor);
     }
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        printf("%d ", tomb[i]);
+    }
+
+    printf("\n");
 
     fclose(fBe);
 
